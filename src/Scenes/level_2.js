f277b63629cbf978afb971ed6c1d866df646f9f5
t4ylo <thpearce@ucsc.edu>
2025-06-01 15:17:05 -0700

    Update level_2.js

--- src/Scenes/level_2.js
+++ src/Scenes/level_2.js
@@ -18,213 +18,143 @@ class Level_2 extends Phaser.Scene {
     }
 
     create() {
-        //audio
-        this.sfx = {};
-        this.sfx.walk = this.sound.add("walk");
-        this.sfx.jump = this.sound.add("jump");
-        this.sfx.coins = this.sound.add("coins");
-        this.sfx.powerup = this.sound.add("powerup");
+            // audio
+            this.sfx = {};
+            this.sfx.walk = this.sound.add("walk");
+            this.sfx.jump = this.sound.add("jump");
+            this.sfx.coins = this.sound.add("coins");
+            this.sfx.powerup = this.sound.add("powerup");
 
-        this.win = false
-        this.playerIsDead = false;
+            this.win = false;
+            this.playerIsDead = false;
 
-        // Create a new tilemap.
-        this.map = this.add.tilemap("level_2", 21, 21, 50, 20);
+            // Track coin collection
+            this.totalCoins = 0;
+            this.collectedCoins = 0;
 
-        // add tileset and bg
-        this.tileset = this.map.addTilesetImage("kenny_tileset_packed", "tilemap_tiles");
+            // Create a new tilemap
+            this.map = this.add.tilemap("level_2", 21, 21, 50, 20);
 
-        this.bg = this.add.tileSprite(
-            0, 0,
-            this.map.widthInPixels, this.map.heightInPixels,
-            "background"
-        ).setOrigin(0, 0).setScrollFactor(0);  
+            // Add tileset and background
+            this.tileset = this.map.addTilesetImage("kenny_tileset_packed", "tilemap_tiles");
 
-        this.bg.setScale(this.SCALE * 2);
+            this.bg = this.add.tileSprite(
+                0, 0,
+                this.map.widthInPixels, this.map.heightInPixels,
+                "background"
+            ).setOrigin(0, 0).setScrollFactor(0);
+            this.bg.setScale(this.SCALE * 2);
 
-        
+            // Create layers
+            this.groundsLayer = this.map.createLayer("grounds", this.tileset, 0, 0);
+            this.lavaNspikesLayer = this.map.createLayer("lava-spikes", this.tileset, 0, 0);
+            this.chainsLayer = this.map.createLayer("chains", this.tileset, 0, 0);
 
-        // Create a layer
-        this.groundsLayer = this.map.createLayer("grounds", this.tileset, 0, 0);
-        this.lavaNspikesLayer = this.map.createLayer("lava-spikes", this.tileset, 0, 0);
+            // Make it collidable
+            this.groundsLayer.setCollisionByProperty({ collides: true });
+            this.lavaNspikesLayer.setCollisionByProperty({ collides: true });
 
-        
-      
-        
-        this.chainsLayer = this.map.createLayer("chains", this.tileset, 0, 0);
-        
-
-        // Make it collidable
-        this.groundsLayer.setCollisionByProperty({
-            collides: true
-        });
-
-        this.lavaNspikesLayer.setCollisionByProperty({
-            collides: true
-        });
-
-
-        // add collectables and make them interact
-
-        this.coins = this.map.createFromObjects("coins", {
-            name: "coin",
-            key: "tilemap_sheet",
-            frame: 78
-        });
-
-        this.coinGroup = this.physics.add.staticGroup();
-
-        this.coins.forEach((coin) => {
-            this.coinGroup.add(coin);
-        });
-
-        this.powerup = this.map.createFromObjects("powerup", {
-            name: "powerup",
-            key: "tilemap_sheet",
-            frame: 376
-        });
-
-        this.powerupGroup = this.physics.add.staticGroup();
-        
-        this.powerup.forEach((p) => {
-            this.powerupGroup.add(p);
-        });
-
-        this.flag = this.map.createFromObjects("flag", {
-            name: "flag",
-            key: "tilemap_sheet",
-            frame: 342
-        });
-
-        this.flagGroup = this.physics.add.staticGroup();
-        
-        this.flag.forEach((f) => {
-            this.anims.create({
-                key: 'flagWave',
-                frames: this.anims.generateFrameNumbers('tilemap_sheet', {frames: [342, 312]}),
-                frameRate: 10,
-                repeat: -1
+            // Coins
+            this.coins = this.map.createFromObjects("coins", {
+                name: "coin",
+                key: "tilemap_sheet",
+                frame: 78
             });
 
-            f.anims.play('flagWave');
-            this.flagGroup.add(f);
-        });
+            this.coinGroup = this.physics.add.staticGroup();
+            this.coins.forEach((coin) => {
+                this.coinGroup.add(coin);
+            });
 
-        
-        // set up player avatar
-        my.sprite.player = this.physics.add.sprite(0, 200, "platformer_characters", "tile_0006.png");
-        my.sprite.player.setCollideWorldBounds(true);
+            this.totalCoins = this.coinGroup.getChildren().length;
 
-        const tileset = this.tileset;
+            this.physics.add.overlap(this.player, this.coinGroup, (player, coin) => {
+                coin.destroy();
+                this.collectedCoins++;
+                this.sfx.coins.play();
+            });
 
-        for (const index in tileset.tileProperties) {
-            const props = tileset.tileProperties[index];
-            if (props.spike) {
-                this.map.setTileIndexCallback(Number(index), () => {
-                    this.handlePlayerDeath();
-                }, this, this.lavaNspikesLayer);
+            // Powerup
+            this.powerup = this.map.createFromObjects("powerup", {
+                name: "powerup",
+                key: "tilemap_sheet",
+                frame: 376
+            });
+
+            this.powerupGroup = this.physics.add.staticGroup();
+            this.powerup.forEach((p) => {
+                this.powerupGroup.add(p);
+            });
+
+            // Flag
+            this.flag = this.map.createFromObjects("flag", {
+                name: "flag",
+                key: "tilemap_sheet",
+                frame: 342
+            });
+
+            this.flagGroup = this.physics.add.staticGroup();
+            this.flag.forEach((f) => {
+                this.anims.create({
+                    key: 'flagWave',
+                    frames: this.anims.generateFrameNumbers('tilemap_sheet', { frames: [342, 312] }),
+                    frameRate: 10,
+                    repeat: -1
+                });
+                f.anims.play('flagWave');
+                this.flagGroup.add(f);
+            });
+
+            // Flag overlap: gate by coin count
+            this.physics.add.overlap(this.player, this.flagGroup, () => {
+                if (this.collectedCoins >= this.totalCoins) {
+                    this.scene.start("endScene");
+                } else {
+                    const msg = this.add.text(this.player.x - 50, this.player.y - 50, "Collect all coins!", {
+                        fontSize: '12px',
+                        fill: '#ff0000'
+                    }).setScrollFactor(0).setDepth(100);
+                    this.time.delayedCall(1000, () => msg.destroy());
+                }
+            });
+
+            // Player setup
+            my.sprite.player = this.physics.add.sprite(0, 200, "platformer_characters", "tile_0006.png");
+            my.sprite.player.setCollideWorldBounds(true);
+
+            const tileset = this.tileset;
+            for (const index in tileset.tileProperties) {
+                const props = tileset.tileProperties[index];
+                if (props.spike) {
+                    this.map.setTileIndexCallback(Number(index), () => {
+                        this.handlePlayerDeath();
+                    }, this, this.lavaNspikesLayer);
+                }
             }
-        }
 
-        
-
-        this.lavaTiles = this.chainsLayer.filterTiles(tile => {
-            return tile.properties.bubble == true;
-        });
-
-        this.lavaTiles.forEach(tile => {
-            const x = this.map.tileToWorldX(tile.x) + this.map.tileWidth / 2;
-            const y = this.map.tileToWorldY(tile.y) + this.map.tileHeight / 2;
-
-            
-
-            this.add.particles(0,0, "kenny-particles", {
-                frame: ['circle_05.png', 'circle_03.png', 'circle_04.png'], 
-                x,
-                y,
-                lifespan: 2500,
-                speedY: { min: -5, max: -30 },
-                scale: { start: 0.05, end: 0 },
-                alpha: { start: 0.5, end: 0 },
-                frequency: 1000,
-                quantity: 1,
-                random: true
-                
-                
+            // Lava particle effect
+            this.lavaTiles = this.chainsLayer.filterTiles(tile => {
+                return tile.properties.bubble == true;
             });
-        });
-        
-        this.physics.add.collider(my.sprite.player, this.lavaNspikesLayer);
-        this.lavaNspikesLayer.setCollisionFromCollisionGroup();
 
-        
+            this.lavaTiles.forEach(tile => {
+                const x = this.map.tileToWorldX(tile.x) + this.map.tileWidth / 2;
+                const y = this.map.tileToWorldY(tile.y) + this.map.tileHeight / 2;
 
-        // Enable collision handling
-        this.physics.add.collider(my.sprite.player, this.groundsLayer);
-        
-        
-        
-
-        
-        // Handle collision detection with coins
-        this.physics.add.overlap(my.sprite.player, this.coinGroup, (player, coin) => {
-            console.log("Collected!");
-            coin.destroy();
-            this.sfx.coins.play()
-        });
-
-        this.physics.add.overlap(my.sprite.player, this.powerupGroup, (player, powerup) => {
-            console.log("PowerUp Collected!");
-            powerup.destroy();
-            this.powerupCollected = true;
-            this.sfx.powerup.play()
-        });
-
-        this.physics.add.overlap(my.sprite.player, this.flagGroup, (player, flag) => {
-            console.log("Complete!");
-            this.win = true
-        });
-        
-
-        // set up Phaser-provided cursor key input
-        cursors = this.input.keyboard.createCursorKeys();
-
-        this.rKey = this.input.keyboard.addKey('R');
-
-        
-
-        
-        // movement vfx
-
-        my.vfx.walking = this.add.particles(0, 0, "kenny-particles", {
-            frame: ['smoke_01.png', 'smoke_02.png'],
-            
-            random: true,
-            scale: {start: 0.03, end: 0.1},
-            
-            maxAliveParticles: 8,
-            lifespan: 350,
-            
-            gravityY: -400,
-            alpha: {start: 1, end: 0.1}, 
-        });
-
-        my.vfx.walking.stop();
-
-        this.canDoubleJump = false;
-        
-
-        // cam
-        this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
-        this.cameras.main.startFollow(my.sprite.player, true, 0.25, 0.25); // (target, [,roundPixels][,lerpX][,lerpY])
-        this.cameras.main.setDeadzone(50, 50);
-        this.cameras.main.setZoom(this.SCALE = 2);
-
-        
-        
-        
-
-    }
+                this.add.particles(0, 0, "kenny-particles", {
+                    frame: ['circle_05.png', 'circle_03.png', 'circle_04.png'],
+                    x,
+                    y,
+                    lifespan: 2500,
+                    speedY: { min: -5, max: -30 },
+                    scale: { start: 0.05, end: 0 },
+                    alpha: { start: 0.5, end: 0 },
+                    frequency: 1000,
+                    quantity: 1,
+                    blendMode: 'ADD'
+                });
+            });
+        }
 
     update() {
         //walking vfx
